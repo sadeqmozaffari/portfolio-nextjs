367217549b515f7db6e3291c5866df6f960ca39d
Sadeq Mozaffari <sadeqmozaffari@gmail.com>
2024-12-14 19:23:17 +0430

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import type {NextConfig} from 'next';
-
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
+
+  reactStrictMode: true,
   images: {
     domains: ['localhost'],
+    unoptimized: true, // Disable default image optimization
   },
+  assetPrefix: isProd ? '/portfolio-nextjs/' : '',
+  basePath: isProd ? '/portfolio-nextjs' : '',
+  output: 'export',
 };
 
 export default nextConfig;
